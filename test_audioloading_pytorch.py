1ade5939298301fc85e3e12b8402492af433e9e4
Fabian-Robert Stöter <fabian-robert.stoeter@audiolabs-erlangen.de>
2019-01-14 15:20:18 +0100

    rename

--- test_audioloading_pytorch.py
+++ test_audioloading_pytorch.py
@@ -9,57 +9,6 @@ import soundfile as sf
 import argparse
 from scipy.io import wavfile
 import librosa
-import numpy as np
-import subprocess as sp
-import os
-DEVNULL = open(os.devnull, 'w')
-
-
-def ffmpeg_load_audio(
-    filename, sr=44100, mono=True, normalize=True, in_type=np.int16, out_type=np.float32
-):
-    channels = 1 if mono else 2
-    format_strings = {
-        np.float64: 'f64le',
-        np.float32: 'f32le',
-        np.int16: 's16le',
-        np.int32: 's32le',
-        np.uint32: 'u32le'
-    }
-    format_string = format_strings[in_type]
-    command = [
-        'ffmpeg',
-        '-i', filename,
-        '-f', format_string,
-        '-acodec', 'pcm_' + format_string,
-        '-ar', str(sr),
-        '-ac', str(channels),
-        '-']
-    p = sp.Popen(command, stdout=sp.PIPE, stderr=DEVNULL, bufsize=4096)
-    bytes_per_sample = np.dtype(in_type).itemsize
-    frame_size = bytes_per_sample * channels
-    chunk_size = frame_size * sr  # read in 1-second chunks
-    raw = b''
-    with p.stdout as stdout:
-        while True:
-            data = stdout.read(chunk_size)
-            if data:
-                raw += data
-            else:
-                break
-    audio = np.fromstring(raw, dtype=in_type).astype(out_type)
-    if channels > 1:
-        audio = audio.reshape((-1, channels)).transpose()
-    if audio.size == 0:
-        return audio, sr
-    if issubclass(out_type, np.floating):
-        if normalize:
-            peak = np.abs(audio).max()
-            if peak > 0:
-                audio /= peak
-        elif issubclass(in_type, np.integer):
-            audio /= np.iinfo(in_type).max
-    return audio, sr
 
 
 def get_files(dir, extension):
@@ -74,22 +23,19 @@ def get_files(dir, extension):
     return audio_files
 
 
-def read_audio(fp, lib="librosa"):
+def audio_to_tensor(fp, lib="librosa"):
     if lib == "torchaudio":
         sig, _ = torchaudio.load(fp)
+        return sig
     elif lib == "soundfile":
         sig, _ = sf.read(fp)
-        sig = torch.FloatTensor(sig).view(1, 1, -1)
     elif lib == "scipy":
         rate, sig = wavfile.read(fp)
-        sig = torch.FloatTensor(sig).view(1, 1, -1)
     elif lib == "librosa":
         sig, sr = librosa.load(fp)
-        sig = torch.FloatTensor(sig).view(1, 1, -1)
     elif lib == "ffmpeg_call":
         sig, sr = ffmpeg_load_audio(fp)
-        sig = torch.FloatTensor(sig).view(1, 1, -1)
-    return sig
+    return torch.FloatTensor(sig).view(1, 1, -1)
 
 
 class AudioFolder(torch.utils.data.Dataset):
@@ -108,7 +54,7 @@ class AudioFolder(torch.utils.data.Dataset):
         self.lib = lib
 
     def __getitem__(self, index):
-        audio = read_audio(random.choice(self.audio_files), lib=self.lib)
+        audio = audio_to_tensor(random.choice(self.audio_files), lib=self.lib)
         return audio
 
     def __len__(self):
