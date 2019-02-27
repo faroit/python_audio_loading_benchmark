c53a26ca17e531abd64fe37d2ecb71e4eff871d2
Fabian-Robert Stöter <fabian-robert.stoeter@audiolabs-erlangen.de>
2019-02-27 22:45:01 +0100

    use mp4 instead of m4a

--- generate_audio.sh
+++ generate_audio.sh
@@ -8,7 +8,7 @@ for k in $(seq -f "%02g" 1 10 151); do
     mkdir $DIR/$k
     for i in $(seq 1 $NBFILES); do
         sox -n -r 44100 $DIR/$k/$i.wav synth "0:$k" whitenoise vol 0.5 fade q 1 "0:$k" 1
-        ffmpeg -i $DIR/$k/$i.wav $DIR/$k/$i.m4a
+        ffmpeg -i $DIR/$k/$i.wav $DIR/$k/$i.mp4
         ffmpeg -i $DIR/$k/$i.wav $DIR/$k/$i.mp3
     done
 done
